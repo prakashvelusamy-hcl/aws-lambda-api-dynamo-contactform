de9009e9255a71d646c8071930ac1974729b26f7
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-07-08 12:38:42 +0530

    fix

--- modules/terraform-aws-apigateway/variables.tf
+++ modules/terraform-aws-apigateway/variables.tf
@@ -2,3 +2,17 @@ variable "lambda_integration_uri_arn" {
   description = "The ARN of the Lambda function to be integrated with API Gateway as the backend."
   type        = string
 }
+
+variable "aws_region" {
+  description = "The AWS region where resources will be deployed."
+  type        = string
+}
+
+variable "route_key" {
+  description = "The route key for the API Gateway HTTP API (e.g., GET /resource or POST /login)."
+  type        = string
+}
+variable "project_tags" {
+  description = "A map of tags for my project"
+  type        = map(string) 
+}
