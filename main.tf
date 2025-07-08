628653e403876cdc367b6440613e6f3e0b24c0ee
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-07-08 14:59:46 +0530

    fix

--- main.tf
+++ main.tf
@@ -4,6 +4,8 @@ module "lambda" {
   aws_lambda_function_name = var.aws_lambda_function_name
   project_tags = var.project_tags
   sns_topic_arn = module.sns.sns_topic_arn
+  dynamodb_name = module.dynamo_db_table.dynamodb_name
+  dynamodb_table_arn = module.dynamo_db_table.dynamodb_table_arn
 }
 
 module "api_gateway" {
