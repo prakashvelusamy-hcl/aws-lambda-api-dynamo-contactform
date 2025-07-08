2aa88f2875e399f810f70089666bfee3544edb38
Prakash_V <96634210+prakashvelusamy-hcl@users.noreply.github.com>
2025-07-08 13:11:08 +0530

    fix

--- modules/terraform-aws-apigateway/main.tf
+++ modules/terraform-aws-apigateway/main.tf
@@ -16,8 +16,6 @@ resource "aws_apigatewayv2_route" "default_route" {
   api_id    = aws_apigatewayv2_api.http_api.id
   route_key = "GET /${var.route_key}"
   target    = "integrations/${aws_apigatewayv2_integration.lambda_integration.id}"
-  authorization_type = "JWT"
-  authorizer_id      = aws_apigatewayv2_authorizer.cognito_authorizer.id
 }
 
 resource "aws_apigatewayv2_stage" "default_stage" {
